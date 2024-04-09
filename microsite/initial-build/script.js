29a761d0a6211f14f0ab50c23e1b67faf10ff9d6
Elle Su <elle.s@wustl.edu>
2024-04-09 15:24:38 -0500

    basic page structure done

---
+++ microsite/initial-build/script.js
@@ -0,0 +1,6 @@
+// Add click event listener to images
+$('.image-container img').on('click', function() {
+    $('body').css('background-color', '#E6DEDF');
+    // Add logic for zooming into the image
+  });
+  
